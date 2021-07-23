f6d8d448d8276b77778888121139a2fb8a1a42fa
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-07-23 17:30:33 +0200

    Update counter.js

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,14 +1,31 @@
 // ---- Codifica per la modifica del DOM ----
 
+// * Sezione della Scritta del Counter *
+// 1. Creo elementi HTML
+
+let sectionCounter = document.createElement("section");
+let divCounter = document.createElement("div");
+let h2Counter = document.createElement("h2");
+// 2. Imposto le classi e ID
+sectionCounter.classList.add("counter");
+
+// 3. Riempimento di contenuto
+divCounter.innerHTML = "<p>0</p>";
+h2Counter.innerHTML = "Counter";
+
+// 4. Inserimento Elementi HTML tramite il DOM
+document.body.prepend(sectionCounter);
+sectionCounter.prepend(divCounter);
+divCounter.after(h2Counter);
+
+// --- Sezione dei bottoni ---
 // 1. Creo elementi HTML
 
 
 // 2. Imposto le classi e ID
 
 
-
 // 3. Riempimento di contenuto
-
 
 
 // 4. Inserimento Elementi HTML tramite il DOM
